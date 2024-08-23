c8e1f37cb57420180fefa70c7546aa5a5f72ddaf
Rishabh Saxena <rishabhsaxena042@gmail.com>
2024-08-23 15:36:58 +0530

    Rishabh

--- app/[lang]/(dashboard)/(home)/dashboard/page-view.jsx
+++ app/[lang]/(dashboard)/(home)/dashboard/page-view.jsx
@@ -16,7 +16,7 @@ const DashboardPageView = ({ trans }) => {
     <div className="space-y-6">
       <div className="flex items-center flex-wrap justify-between gap-4">
         <div className="text-2xl font-medium text-default-800 ">
-          Analytical {trans?.dashboard}
+          Analytics {trans?.dashboard}
         </div>
         <DatePickerWithRange />
       </div>
