a382e5079c17e03ad39e1f406c44b2f097453c2d
Rishabh Saxena <rishabhsaxena042@gmail.com>
2024-08-23 18:42:10 +0530

    For Indian Clients

--- app/[lang]/(dashboard)/(home)/dashboard/components/plan-reports/index.jsx
+++ app/[lang]/(dashboard)/(home)/dashboard/components/plan-reports/index.jsx
@@ -44,63 +44,33 @@ const Planreports = () => {
     const tabsTrigger = [
       {
         value: "all",
-        text: "owners",
+        text: "website owners",
         total: "10,234",
         color: "secondary",
       },
       {
         value: "event",
-        text: "inactive subscribers",
+        text: "Unverified owners",
         total: "536",
         color: "primary",
       },
       {
         value: "conversation",
-        text: "current subscribers",
+        text: "new sign-up",
         total: "21",
         color: "warning",
       },
       {
         value: "newuser",
-        text: "total subscribers",
+        text: "basic plan subscribers",
         total: "3321",
         color: "info",
       },
       {
         value: "all",
-        text: "earning",
+        text: "paid plans subscribers",
         total: "10$",
         color: "secondary",
-      },
-      {
-        value: "event",
-        text: "total earning",
-        total: "300000 $",
-        color: "success",
-      },
-      {
-        value: "conversation",
-        text: "monthly subscriptions",
-        total: "1",
-        color: "info",
-      },
-      {
-        value: "newuser",
-        text: "yearly subscriptions",
-        total: "358321",
-        color: "info",
-      },
-      {
-        value: "conversation",
-        text: "monthly subscriptions",
-        total: "1",
-        color: "info",
-      },
-      {
-        value: "newuser",
-        text: "yearly subscriptions",
-        total: "358321",
-        color: "info",
       },
     ];
   const tabsContentData = [
@@ -131,7 +101,7 @@ const Planreports = () => {
         <div className="flex items-center gap-2 flex-wrap ">
           <div className="flex-1">
             <div className="text-xl font-semibold text-default-900 whitespace-nowrap">
-              Plan Reports
+              Overall Reports
             </div>
             <span className="text-xs text-default-600">
               Demographic properties of your customer
@@ -182,6 +152,64 @@ const Planreports = () => {
               </TabsTrigger>
             ))}
           </TabsList>
+        </Tabs>
+      </CardContent>
+
+      <hr></hr>
+
+      <CardHeader className="border-none pb-0">
+        <div className="flex items-center gap-2 flex-wrap ">
+          <div className="flex-1">
+            <div className="text-xl font-semibold text-default-900 whitespace-nowrap">
+              Indian Reports
+            </div>
+            <span className="text-xs text-default-600">
+              Demographic properties of your customer
+            </span>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="p-1 md:p-5">
+        <Tabs defaultValue="all">
+          <TabsList className="grid grid-cols-2 lg:grid-cols-5 gap-2 md:gap-6 justify-start w-full bg-transparent h-full">
+            {tabsTrigger.map((item, index) => (
+              <TabsTrigger
+                key={`report-trigger-${index}`}
+                value={item.value}
+                className={cn(
+                  "flex flex-col gap-1.5 p-4 overflow-hidden   items-start  relative before:absolute before:left-1/2 before:-translate-x-1/2 before:bottom-1 before:h-[2px] before:w-9 before:bg-primary/50 dark:before:bg-primary-foreground before:hidden data-[state=active]:shadow-none data-[state=active]:before:block",
+                  {
+                    "bg-primary/30 data-[state=active]:bg-primary/30 dark:bg-primary/70": item.color === "primary",
+                    "bg-orange-50 data-[state=active]:bg-orange-50 dark:bg-orange-500": item.color === "warning",
+                    "bg-green-50 data-[state=active]:bg-green-50 dark:bg-green-500": item.color === "success",
+                    "bg-pink-50 data-[state=active]:bg-pink-50 dark:bg-pink-500 ": item.color === "secondary",
+                    "bg-cyan-50 data-[state=active]:bg-cyan-50 dark:bg-cyan-500 ": item.color === "info",
+                  }
+                )}
+              >
+                <span
+                  className={cn(
+                    "h-10 w-10 rounded-full bg-primary/40 absolute -top-3 -right-3 ring-8 ring-primary/30",
+                    {
+                      "bg-primary/50  ring-primary/20 dark:bg-primary dark:ring-primary/40": item.color === "primary",
+                      "bg-orange-200 ring-orange-100 dark:bg-orange-300 dark:ring-orange-400": item.color === "warning",
+                      "bg-green-200 ring-green-100 dark:bg-green-300 dark:ring-green-400": item.color === "success",
+                      "bg-pink-200 ring-pink-100 dark:bg-pink-300 dark:ring-pink-400": item.color === "secondary",
+                      "bg-cyan-200 ring-cyan-100 dark:bg-cyan-300 dark:ring-cyan-400": item.color === "info",
+                    }
+                  )}
+                ></span>
+                <span className="text-sm text-default-800 dark:text-primary-foreground font-semibold capitalize relative z-10">
+                  {" "}
+                  {item.text}
+                </span>
+                <span className={`text-lg font-semibold text-${item.color}/80 dark:text-primary-foreground`}>
+                  {item.total}
+                </span>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
           {/* charts data */}
           {tabsContentData.map((item, index) => (
             <TabsContent key={`report-tab-${index}`} value={item.value}>
